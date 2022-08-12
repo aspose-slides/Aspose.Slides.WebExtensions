5f7af404b60f7757901190898052b9347471d7fd
polonair <fixnim@gmail.com>
2022-08-12 10:33:45 +0300

    avoid XXE vulnerability fix test and setup

--- Aspose.Slides.WebExtensions.Tests/TestDemo.cs
+++ Aspose.Slides.WebExtensions.Tests/TestDemo.cs
@@ -8,14 +8,17 @@ using System.Xml;
 
 namespace Aspose.Slides.WebExtensions.Tests
 {
+    [TestFixture]
     public class TestDemo
     {
         private string TemplatePath = null;
         private string PresentationFilePath = null;
         private string OutputPath = null;
         private string RootDirectory = null;
+        private XmlDocument indexHtml;
+        private XmlNamespaceManager namespaceManager;
 
-        [SetUp]
+        [OneTimeSetUp]
         public void Setup()
         {
             RootDirectory = TestContext.CurrentContext.TestDirectory;
@@ -28,33 +31,34 @@ namespace Aspose.Slides.WebExtensions.Tests
                 WebDocument document = pres.ToSinglePageWebDocument(TemplatePath, OutputPath);
                 document.Save();
             }
+
+            string indexHtmlPath = Path.Combine(OutputPath, "index.html");
+            string indexHtmlContent = File.ReadAllText(indexHtmlPath);
+            indexHtmlContent = FixVideoTag(indexHtmlContent);
+            indexHtml = new XmlDocument();
+            indexHtml.XmlResolver = null;
+            indexHtml.LoadXml(indexHtmlContent);
+            namespaceManager = new XmlNamespaceManager(indexHtml.NameTable);
+            namespaceManager.AddNamespace("html", "http://www.w3.org/1999/xhtml");
         }
         static string FixVideoTag(string input)
         {
             input = Regex.Replace(input, "(<video[^>]+) controls>", "$1 controls=\"\">");
-            input = Regex.Replace(input, "<source[^>]+>", "$1 />");
+            input = Regex.Replace(input, "(<source[^>]+)>", "$1 />");
             return input;
         }
 
         [Test]
         public void TestHeadlineConnector()
         {
-            string indexHtmlPath = Path.Combine(OutputPath, "index.html");
-            Assert.IsTrue(File.Exists(indexHtmlPath));
-            string indexHtmlContent = File.ReadAllText(indexHtmlPath);
-            indexHtmlContent = FixVideoTag(indexHtmlContent);
-            XmlDocument doc = new XmlDocument();
-            doc.LoadXml(indexHtmlContent);
-            var mgr = new XmlNamespaceManager(doc.NameTable);
-            mgr.AddNamespace("html", "http://www.w3.org/1999/xhtml");
-            XmlElement root = doc.DocumentElement;
-            XmlNodeList nodes = root.SelectNodes("//html:div[@id='slide-2147483660-shape-1']", mgr);
+            XmlElement root = indexHtml.DocumentElement;
+            XmlNodeList nodes = root.SelectNodes("//html:div[@id='slide-2147483660-shape-1']", namespaceManager);
 
             Assert.AreEqual(9, nodes.Count);
             foreach(XmlNode node in nodes)
             {
                 string style = node.Attributes["style"].Value;
-                Assert.IsTrue(style.Contains("height: 2px"), "");
+                Assert.IsTrue(style.Contains("height: 2px"), "Headline connector's div block should be 2px high");
             }
         }
     }
