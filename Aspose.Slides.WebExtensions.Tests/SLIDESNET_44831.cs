1873a5cf283b7179ac0fd3c465a274b47c785fe9
alexandermosyagin <alexander.mosyagin@aspose.com>
2025-02-21 15:20:39 +0300

    SLIDESNET-44831: Implemented disabling or enabling font ligatures (#47)
    * SLIDESNET-44831: solution implemented
    
    * SLIDESNET-44831: test added

---
+++ Aspose.Slides.WebExtensions.Tests/SLIDESNET_44831.cs
@@ -0,0 +1,58 @@
+﻿using Aspose.Slides.Export.Web;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Aspose.Slides.WebExtensions.Tests
+{
+    [TestClass]
+    public class SLIDESNET_44831
+
+    {
+        [TestMethod]
+        public void Test_44831()
+        {
+            var RootDirectory = Path.GetFullPath("../../../");
+            var PresentationFilePath = Path.Combine(RootDirectory, "TestData", "SLIDESNET_44831", "SLIDESNET-44831.pptx");
+            var TemplatePath = Path.Combine(RootDirectory, "TestData", "Out", "templates");
+            var OutputPath = Path.Combine(RootDirectory, "TestData", "Out", "SLIDESNET_44831");
+
+            var sourcePath1 = Path.GetFullPath(Path.Combine(RootDirectory, "..", "Aspose.Slides.WebExtensions", "templates", "single-page"));
+            var sourcePath2 = Path.GetFullPath(Path.Combine(RootDirectory, "..", "Aspose.Slides.WebExtensions", "templates", "common"));
+
+            Directory.CreateDirectory(OutputPath);
+            Directory.CreateDirectory(TemplatePath);
+
+            foreach (string dirPath in Directory.GetDirectories(sourcePath1, "*", SearchOption.AllDirectories)) Directory.CreateDirectory(dirPath.Replace(sourcePath1, TemplatePath));
+            foreach (string newPath in Directory.GetFiles(sourcePath1, "*.*", SearchOption.AllDirectories)) File.Copy(newPath, newPath.Replace(sourcePath1, TemplatePath), true);
+
+            foreach (string dirPath in Directory.GetDirectories(sourcePath2, "*", SearchOption.AllDirectories)) Directory.CreateDirectory(dirPath.Replace(sourcePath2, TemplatePath));
+            foreach (string newPath in Directory.GetFiles(sourcePath2, "*.*", SearchOption.AllDirectories)) File.Copy(newPath, newPath.Replace(sourcePath2, TemplatePath), true);
+
+            using (Presentation pres = new Presentation(PresentationFilePath))
+            {
+                WebDocumentOptions options = new WebDocumentOptions
+                {
+                    EmbedImages = true
+                };
+                WebDocument document = pres.ToSinglePageWebDocument(options, TemplatePath, OutputPath);
+                document.Global.Put("disableFontLigatures", true);
+                document.Save();
+                string content = File.ReadAllText(Path.Combine(OutputPath, "index.html"));
+                Assert.IsTrue(content.IndexOf("font-variant-ligatures : none") > 0);
+            }
+            using (Presentation pres = new Presentation(PresentationFilePath))
+            {
+                WebDocumentOptions options = new WebDocumentOptions
+                {
+                    EmbedImages = true
+                };
+                WebDocument document = pres.ToSinglePageWebDocument(options, TemplatePath, OutputPath);
+                document.Global.Put("disableFontLigatures", false);
+                document.Save();
+                string content = File.ReadAllText(Path.Combine(OutputPath, "index.html"));
+                Assert.IsTrue(content.IndexOf("font-variant-ligatures : none") < 0);
+            }
+        }
+    }
+}
